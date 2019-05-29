9d2a35cfef2702a3293c6ec20439b21506664473
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-05-28 22:47:38 -0500

    motor driving works

--- forward/forward.ino
+++ forward/forward.ino
@@ -1,62 +1,42 @@
-#include <Servo.h>
+const int M1DIR = 2;
+const int M1PWM = 3;
+const int M2DIR = 4;
+const int M2PWM = 5;
 
-const int PWM1pin = 3;//A0;   // GPIO pin 14
-const int PWM2pin = 5;
-const int DIR1pin = 2;   // GPIO pin 32
-const int DIR2pin = 4;
-const int PWM1Channel = 1;
-const int PWM2Channel = 0;
-const int freq = 5000;
-const int resolution = 8;
-const int dutyCycle = 5000;
-
-Servo left;
-Servo right;
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(DIR1pin, OUTPUT);
-  pinMode(DIR2pin, OUTPUT);
-  // put your setup code here, to run once:
-  
-  digitalWrite(DIR1pin, 1);
-  digitalWrite(DIR2pin, 1);
+  pinMode(M1DIR, OUTPUT);
+  pinMode(M1PWM, OUTPUT);
+  pinMode(M2DIR, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
 
-
-  right.attach(PWM1pin);
-  left.attach(PWM2pin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  right.write(180);
-  left.write(180);
-  digitalWrite(DIR1pin, HIGH);
-  digitalWrite(DIR2pin, HIGH);
+  digitalWrite(M1DIR,HIGH);
+  digitalWrite(M2DIR,HIGH);
+  analogWrite(M1PWM,255);
+  analogWrite(M2PWM,255);
   Serial.println("forward");
+  delay(1000);
 
-  delay(750);
-  right.write(0);
-  left.write(0);
-  digitalWrite(DIR1pin, 1);
-  digitalWrite(DIR2pin, 1);
+  
+  digitalWrite(M1PWM,0);
+  digitalWrite(M2PWM,0);
   Serial.println("stop");
+  delay(1000);
 
-  delay(750);
-  right.write(180);
-  left.write(180);
-  digitalWrite(DIR1pin, LOW);
-  digitalWrite(DIR2pin, LOW);
+  digitalWrite(M1DIR,LOW);
+  digitalWrite(M2DIR,LOW);
+  analogWrite(M1PWM,255);
+  analogWrite(M2PWM,255);
   Serial.println("backward");
-//
-//  delay(750);
-//  right.write(0);
-//  left.write(0);
-//  digitalWrite(DIR1pin, 1);
-//  digitalWrite(DIR2pin, 1);
-//  Serial.println("stop");
+  delay(1000);
 
-  delay(750);
+  digitalWrite(M1PWM,0);
+  digitalWrite(M2PWM,0);
+  Serial.println("stop");
+  delay(1000);
+
+  
 
 }
